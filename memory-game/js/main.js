ef7fcf2500c6d614b3b13caa08d9a7ae1c661869
Tacy Zachary O <TacyZacharyO@JohnDeere.com>
2020-07-28 23:43:11 -0400

    created game logic

--- memory-game/js/main.js
+++ memory-game/js/main.js
@@ -1,8 +1,22 @@
-console.log("Up and running!");
-let cardOne = "queen";
-let cardTwo = "queen";
-let cardThree = "king";
-let cardFour = "king";
+let result;
+let cards = ["queen","queen","king","king"]; //set variable array
+let cardsInPlay = []; //set empty array
+function checkForMatch(){
+	if (cardsInPlay[0] === cardsInPlay[1]){
+		console.log("You found a match!");
+	} else {
+		console.log("Sorry, try again.");
+	}
+}
+// setting up a flipcard function
+function flipCard(cardId){
+	console.log("User flipped " + cards[cardId]);
+	cardsInPlay.push(cards[cardId]);
+	if (cardsInPlay.length === 2){
+	checkForMatch();
+	}
+}
+flipCard(0);
+flipCard(2);
+// Seeting up variables
 
-console.log("User flipped" + cardOne);
-console.log("User flipped" + cardTwo);

c814e7d0df3e953159789bf756b8a73e7433b36b
Tacy Zachary O <TacyZacharyO@JohnDeere.com>
2020-07-29 12:28:28 -0400

    created game logic

--- memory-game/js/main.js
+++ memory-game/js/main.js
@@ -1,5 +1,26 @@
 let result;
-let cards = ["queen","queen","king","king"]; //set variable array
+let cards = [
+		{
+		rank: "queen",
+		suit: "hearts",
+		cardImage:"images/queen-of-hearts.png",
+		},
+		{
+		rank: "queen",
+		suit: "diamonds",
+		cardImage:"images/queen-of-diamonds.png",
+		},
+		{
+		rank: "king",
+		suit: "hearts",
+		cardImage:"images/king-of-hearts.png",
+		},
+		{
+		rank: "king",
+		suit: "diamonds",
+		cardImage:"images/king-of-diamonds.png",
+		} 
+	];
 let cardsInPlay = []; //set empty array
 function checkForMatch(){
 	if (cardsInPlay[0] === cardsInPlay[1]){
@@ -10,13 +31,15 @@ function checkForMatch(){
 }
 // setting up a flipcard function
 function flipCard(cardId){
-	console.log("User flipped " + cards[cardId]);
-	cardsInPlay.push(cards[cardId]);
+	console.log("User flipped " + cards[cardId].rank);
+	console.log(cards[cardId].cardImage)
+	console.log(cards[cardId].suit)
+	cardsInPlay.push(cards[cardId].rank);
 	if (cardsInPlay.length === 2){
 	checkForMatch();
+	
 	}
 }
 flipCard(0);
 flipCard(2);
 // Seeting up variables
-
